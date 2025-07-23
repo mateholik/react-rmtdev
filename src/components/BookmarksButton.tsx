74bb734ecd5d02f25bc80bd6622eba91b5eb97e6
Vladimir Vaisov <vaisov.vladimir@gmail.com>
2025-07-23 14:54:35 +0300

    feat: update BookmarksButton to handle click outside for closing popover and improve button functionality feat: add min-height to job list for better layout consistency

--- src/components/BookmarksButton.tsx
+++ src/components/BookmarksButton.tsx
@@ -1,11 +1,35 @@
-import { TriangleDownIcon } from "@radix-ui/react-icons";
+import { TriangleDownIcon } from '@radix-ui/react-icons';
+import BookmarksPopover from './BookmarksPopover';
+import { useEffect, useState } from 'react';
 
 export default function BookmarksButton() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        !(event.target as HTMLElement).closest('.bookmarks-popover') &&
+        !(event.target as HTMLElement).closest('.bookmarks-btn')
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
   return (
     <section>
-      <button className="bookmarks-btn">
+      <button
+        onClick={() => setIsOpen((prev) => !prev)}
+        className='bookmarks-btn'
+      >
         Bookmarks <TriangleDownIcon />
       </button>
+      {isOpen && <BookmarksPopover />}
     </section>
   );
 }
