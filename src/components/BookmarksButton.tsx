565c43a284dfd4f4c002cdbc66465e44c9ca514a
Vladimir Vaisov <vaisov.vladimir@gmail.com>
2025-07-24 22:53:38 +0300

    ref instead of css selectors in bookmarks popover

--- src/components/BookmarksButton.tsx
+++ src/components/BookmarksButton.tsx
@@ -1,15 +1,18 @@
 import { TriangleDownIcon } from '@radix-ui/react-icons';
 import BookmarksPopover from './BookmarksPopover';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function BookmarksButton() {
   const [isOpen, setIsOpen] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const popupRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (
-        !(event.target as HTMLElement).closest('.bookmarks-popover') &&
-        !(event.target as HTMLElement).closest('.bookmarks-btn')
+        event.target instanceof HTMLElement &&
+        !popupRef.current?.contains(event.target) &&
+        !buttonRef.current?.contains(event.target)
       ) {
         setIsOpen(false);
       }
@@ -24,12 +27,13 @@ export default function BookmarksButton() {
   return (
     <section>
       <button
+        ref={buttonRef}
         onClick={() => setIsOpen((prev) => !prev)}
         className='bookmarks-btn'
       >
         Bookmarks <TriangleDownIcon />
       </button>
-      {isOpen && <BookmarksPopover />}
+      {isOpen && <BookmarksPopover ref={popupRef} />}
     </section>
   );
 }
--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -1,11 +1,14 @@
 import { useBookmarksContext } from '../lib/hooks';
 import JobList from './JobList';
+import { forwardRef } from 'react';
 
-export default function BookmarksPopover() {
+const BookmarksPopover = forwardRef<HTMLDivElement>(function (_, ref) {
   const { isLoading, bookmarkedJobItems } = useBookmarksContext();
   return (
-    <div className='bookmarks-popover'>
+    <div ref={ref} className='bookmarks-popover'>
       <JobList isLoading={isLoading} jobItems={bookmarkedJobItems} />
     </div>
   );
-}
+});
+
+export default BookmarksPopover;
